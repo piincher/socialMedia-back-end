6874ebb84405962843db41759d7c15b25aee8c6c
Ibrahim Kouma <69568495+piincher@users.noreply.github.com>
2021-10-16 15:09:03 +0800

    setup the server connect to mongoose

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import colors from 'colors';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDb from './config/db';
+
+const app = express();
+dotenv.config();
+connectDb();
+app.use(express.json());
+app.use(
+	cors({
+		origin: 'http://localhost:3000'
+	})
+);
+app.post('/api/register', () => {
+	console.log('register', req.body);
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+	console.log(`server is runing in port ${PORT}`.blue.underline);
+});
