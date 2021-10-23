59a5fdc2defef9872af26789e846a3ff2d26f39d
Ibrahim Kouma <69568495+piincher@users.noreply.github.com>
2021-10-24 07:18:40 +0800

    create a post controller

---
+++ controllers/post.js
@@ -0,0 +1,17 @@
+import Post from '../models/post';
+const createPost = async (req, res) => {
+	const { content } = req.body;
+	if (!content.length) {
+		return res.json({ error: 'content should be provide' });
+	}
+
+	try {
+		const post = new Post({ content, postedBy: req.user._id });
+		post.save();
+		res.json(post);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
+};
+export { createPost };
